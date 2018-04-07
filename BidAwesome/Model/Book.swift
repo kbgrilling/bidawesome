ed1de61952479ac3e9e75c0f20fff12235e1a1e9
Kenneth Blue <kdblue@hotmail.com>
2018-04-06 21:26:33 -0400

    adds platform to book

--- BidAwesome/Model/Book.swift
+++ BidAwesome/Model/Book.swift
@@ -11,16 +11,18 @@ class Book {
   let title: String
   let authors: Array<String>
   let bidPrice: Double
+	let platform: String
   let image: String
   let language: String
   let editor: String
   let description: String
   let id: String?
   
-  init(title: String, authors: [String], bidPrice: Double, image: String, language: String, editor: String, description: String, id: String?) {
+	init(title: String, authors: [String], bidPrice: Double, platform: String, image: String, language: String, editor: String, description: String, id: String?) {
     self.title = title
     self.authors = authors
     self.bidPrice = bidPrice
+	self.platform = platform
     self.image = image
     self.language = language
     self.editor = editor
