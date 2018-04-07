352042341840832d1ee4f3aa491b8b637f05548b
Brody Eller <wviper3@gmail.com>
2018-04-06 22:04:37 -0400

    Finish Database Manager
    addBook - Add or set book data
    getBooks - Returns an array of books

--- BidAwesome/Model/DatabaseManager.swift
+++ BidAwesome/Model/DatabaseManager.swift
@@ -13,29 +13,50 @@ final class DatabaseManager {
 
   private let db = Firestore.firestore()
   
-  private let itemCollection: CollectionReference
+  private let bookCollection: CollectionReference
   
   init() {
-    itemCollection = db.collection("items")
+    bookCollection = db.collection("items")
   }
   
-  func getItem(for id: String, completionHandler: @escaping (_ doc: DocumentSnapshot?, _ err: Error?) -> Void) {
-    let item = itemCollection.document(id)
-    item.getDocument { doc, err in
-      completionHandler(doc, err)
+//  func getItem(for id: String, completionHandler: @escaping (_ doc: DocumentSnapshot?, _ err: Error?) -> Void) {
+//    let item = itemCollection.document(id)
+//    item.getDocument { doc, err in
+//      completionHandler(doc, err)
+//    }
+//  }
+  
+  func addBook(for id: String, book: Book) {
+    bookCollection.document(id).setData([
+      "authors": book.authors,
+      "bidPrice": book.bidPrice,
+      "description": book.description,
+      "editor": book.editor,
+      "language": book.language,
+      "platform": book.platform,
+      "title": book.title
+      ])
+  }
+  
+  func getBooks() -> [Book] {
+    var books = [Book]()
+    bookCollection.getDocuments { doc, err in
+      for docData in (doc?.documents)! {
+        let bookData = docData.data()
+        let book = Book(
+          title: bookData["title"] as! String,
+          authors: bookData["authors"] as! [String],
+          bidPrice: bookData["bidPrice"] as! Double,
+          platform: bookData["platform"] as! String,
+          image: "",
+          language: bookData["language"] as! String,
+          editor: bookData["editor"] as! String,
+          description: bookData["description"] as! String,
+          id: docData.documentID)
+        books.append(book)
+      }
     }
+    return books
   }
-  
-//  func addItem(for id: String, book: Book) {
-//    itemCollection.document(id).setData([
-//      "bidPrice": book.bid,
-//      "description": book,
-//      "editor": book.ide,
-//      ])
-//  }
-  
-//  func getItems() -> [Book] {
-//    var books = [Book]()
-//  }
   
 }
