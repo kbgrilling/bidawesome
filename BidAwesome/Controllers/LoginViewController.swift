546ba788627b1825cfbcaae45eacdfc436ce9e6b
Kenneth Blue <kdblue@hotmail.com>
2018-04-07 12:03:22 -0400

    adds email verification on register and log in

--- BidAwesome/Controllers/LoginViewController.swift
+++ BidAwesome/Controllers/LoginViewController.swift
@@ -36,22 +36,80 @@ class LoginViewController: UIViewController {
   @IBAction func didPressLoginButton(_ sender: UIButton) {
     guard let email = emailTextField.text else { return }
     guard let pwd = passwordTextField.text else { return }
-
+	let alert = UIAlertController(title: "Error", message: "You have an error!", preferredStyle: .alert)
+	let okAction = UIAlertAction(title: "ok", style: .default)
+	alert.addAction(okAction)
     if segmentedControl.selectedSegmentIndex == 0 {
       Auth.auth().createUser(withEmail: email, password: pwd) { user, err in
-        print("\(user?.displayName) Registered Successful")
-        self.dismiss(animated: true, completion: nil)
+		if err != nil {
+			print("err Description \(String(describing: err?.localizedDescription)) -- err code: \(err!._code)")
+			if let errCode = AuthErrorCode(rawValue: err!._code) {
+				/// need to make a password algorithum, username rules, and display for errs
+				switch errCode {
+				case .invalidEmail:
+					print("Invalid Email")
+					self.title = "Try Again"
+					alert.message = "Oh no, email address problem. Review your email address and make sure it is correct! "
+					self.present(alert, animated: true, completion: nil)
+				case .emailAlreadyInUse:
+					alert.message = "Oh no, the email address is already in use by another account. If this is your email address you may already have an account."
+					self.present(alert, animated: true, completion: nil)
+				case .weakPassword:
+					print("Password is not strong")
+					alert.message = "Oh no, Password problem. Your password needs to be stronger at least 6 characters! "
+					self.present(alert, animated: true, completion: nil)
+				default:
+					print("There is an err!")
+					alert.message = "Oh no, there was an err.  Please check your connection and try again. "
+					
+					self.present(alert, animated: true, completion: nil)
+					/*errAlert = UIAlertController(title: "err Occured",
+					message: "There is an err!",
+					preferredStyle: .alert)*/
+				}
+			}
+		}
+		if user != nil {
+			user?.sendEmailVerification() {
+				err in
+				print(err?.localizedDescription as Any)
+			}
+			Auth.auth().signIn(withEmail: email, password: pwd)
+			self.dismiss(animated: true, completion: nil)
+		}
+//        print("\(user?.displayName) Registered Successful")
+//        self.dismiss(animated: true, completion: nil)
       }
-    } else if segmentedControl.selectedSegmentIndex == 1 {
+    } else  {
       Auth.auth().signIn(withEmail: email, password: pwd) { user, err in
-        print("\(user?.displayName) Login Successful")
-        self.dismiss(animated: true, completion: nil)
-      }
-    } else {
-      Auth.auth().createUser(withEmail: email, password: pwd) { user, err in
-        print("\(user?.displayName) Registered Successful")
-        self.dismiss(animated: true, completion: nil)
-      }
+		if err != nil {
+			//print("error Description \(String(describing: err?.localizedDescription)) -- error code: \(err!._code)")
+			alert.message = "Your email and password do not match please try again."
+			let forgotPasswordAction = UIAlertAction(title: "Forgot Password", style: .default) { action in
+				Auth.auth().sendPasswordReset(withEmail: email) { err in
+					if err != nil {
+						if let errorCode = AuthErrorCode(rawValue: err!._code) {
+							switch errorCode {
+							case .invalidSender:
+								print("Invalid Email")
+								self.title = "Try Again"
+								alert.message = "Cannot locate email address. Is this your email address?"
+								self.present(alert, animated: true, completion: nil)
+							default:
+								print("There is an error!")
+								alert.message = "Oh no, there was an error.  Please check your connection and try again."
+								self.present(alert, animated: true, completion: nil)
+							}
+						}
+					}
+				}
+			}
+			alert.addAction(forgotPasswordAction)
+			self.present(alert, animated: true, completion: nil)
+		} else {
+			self.dismiss(animated: true, completion: nil)
+		}
+		}
     }
   }
   
